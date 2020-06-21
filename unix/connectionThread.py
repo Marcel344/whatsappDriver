60ba33d541cc8eac1b3ca8d6b6ca99ccb96ba8d7
Marcel344 <marcel.affi99@gmail.com>
2020-06-22 02:38:34 +0300

    added documentation

--- unix/connectionThread.py
+++ unix/connectionThread.py
@@ -7,22 +7,22 @@ except:
     import http.client as httplib
 
 
-class connectionThread (QThread):
+class connectionThread (QThread): # this is a Thread class responsible for checking internet connection
 
-    connectionInterruptSignal = QtCore.pyqtSignal(bool, name="boolSignal")
+    connectionInterruptSignal = QtCore.pyqtSignal(bool, name="boolSignal") # this is a signal that will trigger the connected function in the main file
 
     def __init__(self):
         super(connectionThread, self).__init__()
 
 
     def run(self):
-        while True :
-            conn = httplib.HTTPConnection("www.google.com", timeout=2)
-            try:
+        while True : # run forever 
+            conn = httplib.HTTPConnection("www.google.com", timeout=2) # establish a connection to google with a 2 seconds delay 
+            try: # try to ping at google 
                 conn.request("HEAD", "/")
-                self.connectionInterruptSignal.emit(True)
+                self.connectionInterruptSignal.emit(True) # send a true bool with the signal signifying that there is internet connection
                 conn.close()
             except:
                 conn.close()
-                self.connectionInterruptSignal.emit(False)
+                self.connectionInterruptSignal.emit(False) # send a false bool with the signal signifying that there is no internet connection
             time.sleep(2) # Check for connection every 2 seconds
